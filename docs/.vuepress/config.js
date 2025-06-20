da9f5e6ac3dbb988d98e2d6bc6fbb0455f1ea794
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2025-06-20 06:25:50 +0000

    i18n(ja-JP): add i18n settings to add japanese docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,26 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  lang: 'en-US',
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    '/': {
+      lang: 'en-US',
 
-  title: 'Novel IL',
-  description: 'The Novel IL Specification',
+      title: 'Novel IL',
+      description: 'The Novel IL Specification',
+    },
+    '/ja/': {
+      lang: 'ja-JP',
 
+      title: 'Novel IL',
+      description: 'Novel IL の公式仕様',
+    },
+  },
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
 
-    navbar: ['/', '/get-started'],
+    navbar: ['/ja/', '/ja/get-started'],
   }),
 
   bundler: viteBundler(),
